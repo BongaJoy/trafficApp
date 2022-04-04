3da1e05124812d7a731fa75f1984dc2d86cd9567
idol <joyce@idolconsulting.co.za>
2022-04-04 14:45:03 +0200

    first commit

---
+++ src/test/java/com/traffic/trafficApp/TrafficAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traffic.trafficApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
